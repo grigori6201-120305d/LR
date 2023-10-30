fbd018f10af125778eea8ec3ef4242acd9d1941c
grigori6201-120305d <147878015+grigori6201-120305d@users.noreply.github.com>
2023-10-30 12:07:21 +0400

    Update Pg9.sql

--- Лр2/Pg9.sql
+++ Лр2/Pg9.sql
@@ -1,6 +1,3 @@
 -- Active: 1697233781638@@127.0.0.1@3306@cd
-use cd;
-SELECT firstname, surname, joindate 
-FROM members 
-ORDER BY joindate DESC 
-LIMIT 1
+USE cd;
+SELECT surname, firstname FROM members WHERE joindate > '2012-08-31';
