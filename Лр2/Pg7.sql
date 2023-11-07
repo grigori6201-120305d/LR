827af8777b0de0c7d36c69350b25e88cb46aab58
Yia <dmitrienkoga03@yandex.ru>
2023-11-07 10:24:43 +0400

    lll

--- Лр2/Pg7.sql
+++ Лр2/Pg7.sql
@@ -1,2 +1,2 @@
-USE cd; /*с помощью создания массива данных, проверяется равенство facid равное 5 или 1*/
-SELECT * FROM facilities WHERE facid = 5  OR facid = 1;
+USE cd; /*с помощью создания массива данных, проверяется равенство facid равное 5 или 1*/ 
+SELECT * FROM facilities WHERE facid IN (1,5)
--- Лр2/Pg9.sql
+++ Лр2/Pg9.sql
@@ -1,3 +1,3 @@
 -- Active: 1697233781638@@127.0.0.1@3306@cd
 USE cd; /*через оператор > вцыводятся члены клуба зарегистрированные с 1 сентября 2012 года*/
-SELECT surname, firstname FROM members WHERE joindate > '2012-08-31'; 
+SELECT memid, surname, firstname FROM members WHERE joindate > '2012-08-31'; 
