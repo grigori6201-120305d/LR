6d1deda2cd4d00ad28281dcd827da1f040963f30
Yia <dmitrienkoga03@yandex.ru>
2023-11-07 10:44:09 +0400

    Update 2-7

--- Лр2/Pg7.sql
+++ Лр2/Pg7.sql
@@ -1,2 +1,2 @@
 USE cd; /*с помощью создания массива данных, проверяется равенство facid равное 5 или 1*/ 
-SELECT * FROM facilities WHERE facid IN (1,5)
+SELECT FROM facilities WHERE facid IN (1,5)
