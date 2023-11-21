f0f6b93283662513cc4b19dffb00f32608d0d381
Yia <dmitrienkoga03@yandex.ru>
2023-11-21 14:26:09 +0400

    4444

--- Лр4/2.sql
+++ Лр4/2.sql
@@ -1,8 +1,8 @@
 -- Active: 1697233781638@@127.0.0.1@3306@cd
 /*Добавить (одновременно)  два сквош-корта (2 и 3 == ‘Squash Court 2’, ‘Squash Court 3’)  со стоимостью создания = 5000--
- --стоимостью обслуживания = 80, стоимостью аренды для членов клуба = 3.5, а гостей*/
-USE cd;
-ALTER TABLE facilities
+*/
+
+ ALTER TABLE facilities
 MODIFY membercost
 DECIMAL(10,1);
 
@@ -14,3 +14,8 @@ INSERT INTO facilities (facid, facility, membercost, guestcost, initialoutlay, m
 VALUES (10, 'Squash Court 2', 3.5, 17.5, 5000, 80),
        (11, 'Squash Court 3', 3.5, 17.5, 5000, 80);
 SELECT * FROM facilities;
+
+
+/*
+DELETE FROM facilities
+WHERE facid IN(10,11);
--- Лр4/4.sql
+++ Лр4/4.sql
@@ -1,7 +1,13 @@
 -- Active: 1697233781638@@127.0.0.1@3306@cd
 USE cd;
 /*Реальные затраты на создание корта 2 (facid =1) составили 10000. Обновите данные в таблице, чтобы учесть это изменение*/
+
+
+ALTER TABLE facilities
+MODIFY initialoutlay
+DECIMAL(10,2);
+
 UPDATE facilities
-SET initialoutlay = 10000
-WHERE facid = 1;
+SET initialoutlay = 10000 
+WHERE facid = 1 AND initialoutlay = initialoutlay + 0.02;
 SELECT * FROM facilities;
