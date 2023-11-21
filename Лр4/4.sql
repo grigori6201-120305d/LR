432dce5511767fe8323009871bd3a55cd32c98d2
Yia <dmitrienkoga03@yandex.ru>
2023-11-21 14:37:08 +0400

    444

--- Лр4/4.sql
+++ Лр4/4.sql
@@ -2,12 +2,12 @@
 USE cd;
 /*Реальные затраты на создание корта 2 (facid =1) составили 10000. Обновите данные в таблице, чтобы учесть это изменение*/
 
-
 ALTER TABLE facilities
 MODIFY initialoutlay
 DECIMAL(10,2);
 
 UPDATE facilities
-SET initialoutlay = 10000 
-WHERE facid = 1 AND initialoutlay = initialoutlay + 0.02;
-SELECT * FROM facilities; facid
+SET initialoutlay = 10000 + 0.02
+WHERE facid = 1; 
+
+SELECT * FROM facilities;
