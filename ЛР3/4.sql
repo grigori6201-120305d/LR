fd1b75f869317778e2abc6a2d73af6ec2b85592d
Yia <dmitrienkoga03@yandex.ru>
2023-11-21 11:00:47 +0400

    Лаба3 - 4 Update

--- ЛР3/4.sql
+++ ЛР3/4.sql
@@ -7,5 +7,5 @@ SELECT m1.memid AS 'id',
     m2.firstname AS 'рекомендовали',
     m2.surname AS 'рекомендовали'
 FROM members m1 
-LEFT JOIN members m2 ON m1.recommendedby = m2.memid 
+JOIN members m2 ON m1.recommendedby = m2.memid 
 ORDER BY  m1.firstname, m1.surname
