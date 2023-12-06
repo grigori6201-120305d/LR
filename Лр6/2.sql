079dd116196402b245ae623d81513aaf9d8e8048
Yia <dmitrienkoga03@yandex.ru>
2023-12-06 17:39:00 +0400

    Лаба6 - 2,3

--- Лр6/2.sql
+++ Лр6/2.sql
@@ -1,7 +1,7 @@
 -- Active: 1697233781638@@127.0.0.1@3306@cd
 /*Выберите количество аренд объектов клуба в каждый месяц.*/
 USE cd;
-SELECT YEAR(b.starttime) AS 'Год', MONTH(b.starttime) AS 'Месяц', f.facility AS 'Название объекта', COUNT(*) AS 'Количество аренд в каждый месяц'
-FROM bookings b
+SELECT YEAR(b.starttime)AS YEAR,MONTH(b.starttime) AS MONTH, COUNT(*) AS rental_count
+FROM bookings b 
 JOIN facilities f ON b.facid = f.facid
-GROUP BY YEAR(starttime), MONTH(starttime), facility;
+GROUP BY MONTH(b.starttime), YEAR(b.starttime);
--- Лр6/3.sql
+++ Лр6/3.sql
@@ -1,10 +1,13 @@
 -- Active: 1697233781638@@127.0.0.1@3306@cd
 /*Выберите процент использования объектов по месяцам, упорядочив по возрастанию*/
 USE cd;
-SELECT 
-    MONTH(b.starttime) AS months, f.facility,
-    (SUM(b.slots) / (COUNT(DISTINCT b.facid) * 720 * COUNT(DISTINCT YEAR(b.starttime)))) * 100 AS usage_percent 
-FROM bookings b
-JOIN facilities f ON b.facid = f.facid 
-GROUP BY months, f.facility
+WITH slots AS (SELECT facility, SUM(b.slots) AS bookcost,   
+    DATE_FORMAT(b.starttime, '%y.%m') AS YearMonth
+    FROM facilities f
+    JOIN bookings b ON b.facid = f.facid
+    GROUP BY f.facid, YearMonth)
+SELECT slot1.facility AS facility, ROUND(slot1.bookcost / SUM(slot2.bookcost) * 100, 1) AS usage_percent, slot1.YearMonth
+FROM slots AS slot1
+JOIN slots AS slot2 ON slot1.YearMonth = slot2.YearMonth
+GROUP BY slot1.facility, slot1.bookcost, slot1.YearMonth
 ORDER BY usage_percent ASC;
